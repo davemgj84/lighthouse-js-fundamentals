4d44665e7a9d2dcd9f95eb4dc1d963f719e0cad3
davemgj84 <davemgj@gmail.com>
2020-07-22 03:20:01 +0000

    Odd Checker Quiz

---
+++ odd.js
@@ -0,0 +1,11 @@
+//Odd Checker - Assignment
+
+const isOdd = function (num) {
+  return num % 2 !== 0;
+}
+
+//testing:
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("23 is odd: " + isOdd(23));
