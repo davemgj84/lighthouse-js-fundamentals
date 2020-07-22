63dc4d76b0aa8d2f6edca7f60a7d6f10ed67ef96
davemgj84 <davemgj@gmail.com>
2020-07-22 02:56:58 +0000

    Declaring and calling functions example

---
+++ hello.js
@@ -0,0 +1,17 @@
+//Declaring and calling functions
+
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+const sayHello  = function (name) {
+  console.log("Hello " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
