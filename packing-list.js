5ee2114a5b5d414ccc10bdfc3433b34a74b3b155
davemgj84 <davemgj@gmail.com>
2020-07-29 03:19:17 +0000

    adding ice-cream exercise

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,7 @@
 // Packing-List Exercise - Iterating Over Arrays:
 
+// For Loops:
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
@@ -8,4 +10,14 @@ for (let i = 0; i < packingList.length; i++) {
 }
 
 packingList[3];
+
+
+//While Loops:
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
 
